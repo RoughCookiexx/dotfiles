edb2cfbeef86ab612cbedb48ed9d74cee31e7081
Tommy <roughcookie@gmail.com>
2025-08-21 12:55:37 -0400

    fuck it just hardcode each file we arent writing this for nasa

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -2,28 +2,29 @@
 
 set -euo pipefail
 
-SOURCE_DIRS=(hypr)
-DEST_DIR="$HOME/.config"
+SOURCE_FILES=(
+	".config/hypr/hyprland.conf"
+	".config/hypr/hyprpaper.conf"
+)
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+DEST_DIR="$HOME"
 BACKUP_DIR="$HOME/.config_backup_$(date +%s)"
 mkdir -p "$BACKUP_DIR"
 
-for dir in "${SOURCE_DIRS[@]}"; do
-	src="$(pwd)/$dir"
-	dest="$DEST_DIR/$dir"
+echo "Backing up existing files (-) and creating symlinks for new files (+)"
+for file in "${SOURCE_FILES[@]}"; do
+	src="$SCRIPT_DIR/$file"
+	dest="$DEST_DIR/$file"
 
 	mkdir -p "$(dirname "$dest")"
-	
-	echo "Backing up exist files (-) and creating symlinks for new files (+)"
-	for file in "$src"/*; do
-		target="$dest/$(basename "$file")"
 
-		if [ -e "$target" ] && [ ! -L "$target" ]; then
-			echo "    -$target"
-			mv "$target" "$BACKUP_DIR/"
-		fi
+	if [ -e "$dest" ]; then
+		echo "    -$dest"
+		mv "$dest" "$BACKUP_DIR/"
+	fi
 
-		echo "    +$target"
-		ln -sf "$file" "$target"
-	done
+	echo "    +$dest"
+	ln -sf "$file" "$dest"
 done
 
